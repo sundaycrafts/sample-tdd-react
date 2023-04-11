ad9301ae5795e13e90e3f5f05c547750fb4adba7
sundaycrafts <sundaycrafts@users.noreply.github.com>
2023-04-11 22:14:59 +0200

    npx storybook@latest init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
-/** @type {import('@types/eslint').Linter.BaseConfig} */
+/** @type {import("@types/eslint").Linter.BaseConfig} */
 module.exports = {
   extends: [
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/jest-testing-library",
     "prettier",
+    "plugin:storybook/recommended",
   ],
   env: {
     "cypress/globals": true,
