935d3988da9bb54790323277525bb660bc162661
sundaycrafts <sundaycrafts@users.noreply.github.com>
2023-04-14 16:22:00 +0200

    fix: storybook path error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../component/**/*.stories.@(js|jsx|ts|tsx)",
+    "../app/component/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
